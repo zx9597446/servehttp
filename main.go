34b3df5f994bdddbfceac827afde2ece98dc3125
admin <admin@macmini.local>
2014-09-04 14:26:30 +0800

    change

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import "github.com/codegangsta/martini"
+import "io/ioutil"
 import "log"
 import "net/http"
 
-const defaultAddr = ":9502"
+const defaultAddr = ":80"
+
+func viascan() string {
+	b, err := ioutil.ReadFile("viascan.html")
+	if err != nil {
+		log.Panicln(err, b)
+	}
+	return string(b)
+}
 
 func main() {
 	m := martini.Classic()
 	m.Use(martini.Static("."))
+	m.Get("/viascan", viascan)
 	log.Printf("serve http %s\n", defaultAddr)
 	log.Fatal(http.ListenAndServe(defaultAddr, m))
 }
